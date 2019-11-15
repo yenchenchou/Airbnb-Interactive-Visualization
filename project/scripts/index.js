90fe27d6919971886513c3a883d7b10b64199836
[weifanchen] <[ap248166@gmail.com]>
2019-11-14 20:16:54 -0800

    data update

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -5,25 +5,52 @@ var svg = d3.select("#map")
             .attr('width',width)
             .attr('height',height);
 
-d3.json("./data/dataset/california-counties.geojson").then(function (json) {
 
-    var projection = d3.geoMercator().fitSize([width, height], json);
+var promises = [];
+promises.push(d3.json("./data/dataset/california-counties.geojson"))
+promises.push(d3.csv("./data/dataset/df_california_filtered_cleaned.csv"))
+
+Promise.all(promises).then(function(values){
+    var map = values[0];
+    var data = values[1];
+    var test = data.map(d => {
+        var rObj = {};
+        rObj['host_id'] =d.host_id
+        rObj['coordinate'] = [+d.longitude,+d.latitude];
+        return rObj;
+        })
+    //var test = Object.assign(new Map(data.map(d=> [d.latitude,d.longitude])))
+
+    var projection = d3.geoMercator().fitSize([width, height], map);
     var path = d3.geoPath().projection(projection);
 
-    counties = svg.append('g')
+    var counties = svg.append('g')
         .attr('class','counties')
         .style('fill','whitesmoke');
      
         
     counties.selectAll('path')
-        .data(json.features)
+        .data(map.features)
         .enter()
         .append("path")
         .attr('class','county')
         .attr("d", path)
         .style('stroke','darkgrey')
         .on("click", clicked);
-
+    
+    
+     var spot=svg.append('g')
+        .attr('class','house_spot')
+    
+    spot.selectAll('circle')
+         .data(test)
+         .enter()
+         .append('circle')
+         .attr('r',0.5)
+         .attr('cx',d=>projection(d.coordinate)[0]) 
+         .attr('cy',d=>projection(d.coordinate)[1]) 
+         .attr('fill','black')
+         .style('opacity','0.5');
 
     function clicked(d) {
         var x, y, k;
@@ -48,8 +75,13 @@ d3.json("./data/dataset/california-counties.geojson").then(function (json) {
             .duration(750)
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
             .style("stroke-width", 1.5 / k + "px");
-        }
-          
+        
+        spot.transition()
+            .duration(750)
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+            .style("stroke-width", 1.5 / k + "px");
+        
+    }
 
 
 // a9-ex5
