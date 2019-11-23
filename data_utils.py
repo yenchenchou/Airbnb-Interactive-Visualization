e285ff8108de1910b2cb61650fff14eaf1d22d9b
CHOUYENCHEN <intelchou@gmail.com>
2019-11-22 19:08:37 -0800

    update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -13,7 +13,7 @@ import seaborn as sns
 
 pd.set_option("display.max_columns", 500)
 
-all_file_list = glob.glob('/Users/yc/Desktop/Airbnb' + "/*.csv")
+all_file_list = glob.glob('/Users/yc/Desktop/Airbnb/raw_data' + "/*.csv")
 path = '/Users/yc/Desktop/OneDrive/USC/課程syllabus+resource/INF554/a5-undefined/project/data/dataset/'
 
 def get_data(all_file_list):
@@ -32,10 +32,11 @@ def get_data(all_file_list):
 
 
 def hotel_describe_words(path):
-    with open(path + 'describe_word.txt', 'r') as f:
+    with open(path + 'describe.txt', 'r') as f:
         describe_word = f.read().split(',')
         describe_word = [word.strip() for word in describe_word]
     return describe_word
+
 
 def get_keyword(df):
     ls = []
@@ -50,32 +51,19 @@ def get_keyword(df):
 
 
 def data_cleaning(df):
-    # first time column drop by number of not null
-    filter_column_name = list(df.isnull().sum().sort_values(ascending = True).index[:50])
-    filter_column_name.append('amenities')
+    filter_column_name = ['host_id', 'name', 'latitude', 'longitude', 'room_type', 'price',
+                            'minimum_nights', 'reviews_per_month', 'number_of_reviews',
+                            'description', 'amenities', 'property_type', 'accommodates',
+                            'availability_365', 'instant_bookable', 'city']
     df = df[filter_column_name]
-    
-    # second time column drop
-    df.drop(['calculated_host_listings_count', 
-             'minimum_nights',
-             'neighbourhood',
-             'last_review'],
-           axis = 1,
-           inplace = True)
-
-    numeric_list = ['accommodates', 'number_of_reviews', 'reviews_per_month']
-    cat_list = ['room_type', 'room_type', 'name', 'instant_bookable']
-    cat1, cat2 = 'extra pillows and blankets', 'carbon monoxide detector'
- 
-    df.dropna(subset = ['longitude', 'latitude', 'price'], inplace = True)    
-    df[numeric_list] = df[numeric_list].fillna(0)
-    df.loc[:,cat1:cat2] = df.loc[:,cat1:cat2].fillna('Unknown')
-    df[cat_list] = df[cat_list].fillna('Unknown')
-    df['description'] = get_keyword(df)
-    df['description'] = df['description'].fillna('<None>')
-    df['amenities'] = df['amenities'].fillna('Unknown')
+    df.dropna(subset = ['longitude', 'latitude', 'price'], inplace = True) 
     df['price'] = [float(re.sub(r"[^0-9]", "", row)) for row in df['price']]
-    
+    df['description'] = get_keyword(df)
+    df['description'].fillna('<None>', inplace = True)
+    df['amenities'] = [re.sub(r"[\"\{\}]", "", row) for row in df['amenities']]
+    df['amenities'].fillna('Unknown', inplace = True)
+    df.fillna(0, inplace = True)
+
     return df
 
 if __name__ == '__main__':
@@ -84,5 +72,5 @@ if __name__ == '__main__':
     describe_word = hotel_describe_words(path)
     df_new = data_cleaning(df)
     save_path = '/Users/yc/Desktop/OneDrive/USC/課程syllabus+resource/INF554/a5-undefined/project/data/dataset'
-    df_new.to_csv(os.path.join(save_path ,"df_california_filtered_cleaned_2.csv"), index = False)
+    df_new.to_csv(os.path.join(save_path ,"df.csv"), index = False)
     print('complete!')
