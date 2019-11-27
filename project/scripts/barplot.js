be0f44bada647321e9a36ee3cb3327ea14fca3e5
CHOUYENCHEN <intelchou@gmail.com>
2019-11-27 10:13:09 -0800

    js files code cleaning

--- project/scripts/barplot.js
+++ project/scripts/barplot.js
@@ -10,12 +10,11 @@ d3.json('data/dataset/room_type_price.json').then(data => {
     const margin = {top: 20, right: 20, bottom: 45, left: 120};
     const graphWidth = 800 - margin.left - margin.right;
     const graphHeight = 300 - margin.top - margin.bottom;
-
     const graph = svg.append('g')
         .attr('width', graphWidth)
         .attr('height', graphHeight)
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
+    // prepare scale for rect/color
     const x = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.price)])
         .range([0, graphWidth]); 
@@ -26,41 +25,37 @@ d3.json('data/dataset/room_type_price.json').then(data => {
         .paddingOuter(0.2);
     const myColor = d3.scaleOrdinal().domain(data.map(item => item.room_type))
         .range(['#FF5A5F', '#00A699', '#FC642D']);
-    console.log(myColor)
 
-    // event handler
+    // mouse hover event handler
     const handleMouserover = (d, i, n) => {
         d3.select(n[i])
         .transition().duration(10)
             .attr('stroke', '#767676')
             .attr('stroke-width', '3px')
-    }
+    };
     const handleMouseout = (d, i, n) => {
         d3.select(n[i])
         .transition().duration(10)
             .attr('stroke','#ffd1d2')
             .attr('stroke-width', '0px')   
-    } 
+    } ;
 
-
-    // Tooltip setupb order-radius: 25px;
+    // tooltip function and content setup
     const tip = d3.tip()
     .attr('class', 'tip_card')
     .html((d, i, n) => {
         let content = `<p class=bubble_point>${'$' + d.price.toFixed(2)}</p>`;;
         return content;
-    }).direction('se')
-    graph.call(tip)
+    }).direction('se');
+    graph.call(tip);
 
-
-    // join the data to circs
+    // join the data to rects
     const rects = graph.selectAll('rect')
-    .data(data);
-    // append the enter selection to the DOM
-    rects.enter()
+        .data(data)
+        .enter()
         .append('rect')
         .attr('height', y.bandwidth())
-        .attr('fill', function(d){return myColor(d.room_type)})
+        .attr('fill', d => myColor(d.room_type))
         .attr('x', 0)
         .attr('y', d => y(d.room_type))
         .attr('stroke-width', '0px')
@@ -76,33 +71,25 @@ d3.json('data/dataset/room_type_price.json').then(data => {
         .on("mouseout", (d, i, n) => {
             tip.hide();
             handleMouseout(d, i, n);
-        })
-
+        });
     // create axes groups
     const xAxisGroup = graph.append('g')
         .attr("class", "axis_x")
-        .attr('transform', `translate(0, ${graphHeight})`)
+        .attr('transform', `translate(0, ${graphHeight})`);
     const yAxisGroup = graph.append('g')
-        .attr("class", "axis_y")
-
-    // create & call axesit
+        .attr("class", "axis_y");
     const xAxis = d3.axisBottom(x)
         .ticks(7)
         .tickFormat(d => '$' + d);
     const yAxis = d3.axisLeft(y)
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
-    xAxisGroup.selectAll('text')
-        // .attr('fill', '#767676')
-    yAxisGroup.selectAll('text')
-
     // text label for the x axis
     svg.append("text")             
         .attr("transform", `translate(${graphWidth/2 + margin.left}, ${graphHeight + margin.top + 40})`)
+        .attr('font-family', 'Arial')
         .attr('fill', '#767676')
+        .text("Average Price")
         .style('font-size', '16px')
-        .style("text-anchor", "middle")
-        .text("Avg. Price")
-        .attr('font-family', "Arial")
-        .attr('fill', '#767676');
+        .style("text-anchor", "middle");
 });
--- project/scripts/bubbleplot.js
+++ project/scripts/bubbleplot.js
@@ -22,50 +22,45 @@ d3.csv('data/dataset/df.csv').then(data => {
     const margin = {top: 20, right: 20, bottom: 50, left: 120};
     const graphWidth = 800 - margin.left - margin.right;
     const graphHeight = 600 - margin.top - margin.bottom;
-
     const graph = svg.append('g')
         .attr('width', graphWidth)
         .attr('height', graphHeight)
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    // prepare for the bubble 
+    // prepare scale for bubble/color/legend
     const x = d3.scaleLinear()
-    .domain([0, d3.max(priceAvgAmount, d => d.value.avg_number_of_reviews)])
-    .range([0, graphWidth]);
-
+        .domain([0, d3.max(priceAvgAmount, d => d.value.avg_number_of_reviews)])
+        .range([0, graphWidth]);
     const y = d3.scaleLinear()
-    .domain([0, d3.max(priceAvgAmount, d => d.value.avg_availability_365)])
-    .range([graphHeight, 0]);
-
+        .domain([0, d3.max(priceAvgAmount, d => d.value.avg_availability_365)])
+        .range([graphHeight, 0]);
     const z = d3.scaleLinear()
-    .domain([0, d3.max(priceAvgAmount, d => d.value.cnt_house)])
-    .range([4, 30]);
-
+        .domain([0, d3.max(priceAvgAmount, d => d.value.cnt_house)])
+        .range([4, 30]);
     const legend_size = d3.scaleSqrt()
-    .domain([0, d3.max(priceAvgAmount, d => d.value.cnt_house)])
-    .range([4, 30]);
-
+        .domain([0, d3.max(priceAvgAmount, d => d.value.cnt_house)])
+        .range([4, 30]);
     const myColor = d3.scaleLinear()
         .domain([20, 300])
-        .range(["#00ccbb", "#FF5A5F"]); // ["#00A699", "#FF5A5F"]
+        .range(["#00ccbb", "#FF5A5F"]);
 
-    // event handler
+    // mouse hover event handler
     const handleMouserover = (d, i, n) => {
         d3.select(n[i])
-        .transition().duration(10)
-            .style('opacity', 1)
+            .transition().duration(10)
             .attr('stroke', '#767676')
             .attr('stroke-width', '3.5px')
-    }
+            .style('opacity', 1)
+    };
     const handleMouseout = (d, i, n) => {
         d3.select(n[i])
-        .transition().duration(10)
+            .transition().duration(10)
             .attr('stroke','white')
             .attr('stroke-width', '1px')
             .style("opacity", 0.5)      
-    } 
+    };
 
-    // Tooltip setup
+    // tooltip function and content setup
     const tip = d3.tip()
     .attr('class', 'tip_card')
     .html((d, i, n) => {
@@ -74,23 +69,22 @@ d3.csv('data/dataset/df.csv').then(data => {
         content += `<p>days available: ${d.value.avg_availability_365.toFixed(0)}</p>`;
         content += `<p style='color:#fff;'>avg price: ${d.value.avg_price.toFixed(0)}</p>`;
         return content;
-    }).direction('se')
-    graph.call(tip)
+    }).direction('se'); // south east direction
+    graph.call(tip);
 
-    // join the data to circs
+    // join the data to cirlces
     const circ = graph.selectAll('circle')
         .data(priceAvgAmount)
         .enter()
         .append('circle')
         .attr("class", "bubbles")
-        .attr("cx", function (d) { return x(d.value.avg_number_of_reviews); } )
-        .attr("cy", function (d) { return y(d.value.avg_availability_365); } )
-        .attr("r", function (d) { return z(d.value.cnt_house); } )
+        .attr("cx", d => x(d.value.avg_number_of_reviews))
+        .attr("cy", d => y(d.value.avg_availability_365))
+        .attr("r", d => z(d.value.cnt_house))
         .attr('stroke','white')
         .attr('stroke-width', '1px')
-        .style("fill", function (d) { return myColor(d.value.avg_price); })
+        .style("fill", d => myColor(d.value.avg_price))
         .style("opacity", 0.5 )
-
     graph.selectAll('circle')
         .on("mouseover", (d, i, n) => {
             tip.show(d, n[i]);
@@ -101,70 +95,71 @@ d3.csv('data/dataset/df.csv').then(data => {
             handleMouseout(d, i, n);
         })
 
-    //// legend setup
+    // legend setup (bubble)
     const legendData = [1,5000,20000]
     graph.selectAll("legend")
         .data(legendData)
         .enter()
         .append("circle")
         .attr("cx", graphWidth - 100)
-        .attr("cy", function(d){ return 60 - legend_size(d) } )
-        .attr("r", function(d){ return legend_size(d) })
-        .style("fill", "none")
+        .attr("cy", d => 60 - legend_size(d))
+        .attr("r", d => legend_size(d))
         .attr("stroke", "#767676")
         .attr('stroke-width', '2px')
-        .style("opacity", 0.5)
-    // Add legend: segments
+        .style("fill", "none")
+        .style("opacity", 0.5);
+    // legend segments line (bubble)
     svg.selectAll("legend")
         .data(legendData)
         .enter()
         .append("line")
             .attr('x1', graphWidth + 70)
-            .attr('x2', function(d){ return graphWidth + legend_size(d) + 20})
-            .attr('y1', function(d){ return 80 - legend_size(d) } )
-            .attr('y2', function(d){ return 80 - legend_size(d) } )
+            .attr('x2', d => graphWidth + legend_size(d) + 20)
+            .attr('y1', d => 80 - legend_size(d))
+            .attr('y2', d => 80 - legend_size(d))
             .attr('stroke', '#767676')
             .attr('stroke-width', '2px')
-            .style('stroke-dasharray', ('2,2'))
-    // Add legend: labels
+            .style('stroke-dasharray', ('2,2'));
+    // legend labels (bubble)
     svg.selectAll("legend")
         .data(legendData)
         .enter()
         .append("text")
             .attr('x', graphWidth + 80)
-            .attr('y', function(d){ return 80 - legend_size(d) })
-            .text( function(d){ return d } )
-            .style("font-size", 12)
+            .attr('y', d => 80 - legend_size(d))
+            .text(d => d)
             .attr('alignment-baseline', 'middle')
             .attr('font-family', 'Arial')
             .attr("fill", "#767676")
-    // Add square
-    svg.selectAll("legend")
-        .data(legendData)
-        .enter()
-        .append("rect")
-            .attr('x', graphWidth - 20)
-            .attr('y', margin.top)
-            .attr("width", 150)
-            .attr("height", 160)
-            .attr('stroke', '#767676')
-            .attr('stroke-width', '1.5px')
-            .style('stroke-dasharray', ('2,2'))
-            .attr("fill", "none")
-    // Add legend text
+            .style("font-size", 12);
+    // legend text (bubble)
     svg.append("text")             
-        .attr("transform", `translate(${graphWidth - 20}, ${margin.top - 1})`)
-        .attr('fill', '#767676')
-        .style('font-size', '12px')
+        .attr("transform", `translate(${graphWidth - 15}, ${margin.top - 1})`)
         .text("House Counts")
-        .attr('font-family', 'Arial');
-    // Color legend.
-    data_for_legend = [
+        .attr('fill', '#767676')
+        .attr('font-family', 'Arial')
+        .style('font-size', '12px');
+    // Add square for legend
+    // svg.selectAll("legend")
+    //     .data(legendData)
+    //     .enter()
+    //     .append("rect")
+    //         .attr('x', graphWidth - 20)
+    //         .attr('y', margin.top)
+    //         .attr("width", 150)
+    //         .attr("height", 160)
+    //         .attr('stroke', '#767676')
+    //         .attr('stroke-width', '1.5px')
+    //         .style('stroke-dasharray', ('2,2'))
+    //         .attr("fill", "none")
+
+    // legend setup (colored square, which represent the price)
+    const data_for_legend = [
         {"color":"#ff5a5f","value":0,"price":1000},
         {"color":"#ffb3b5","value":20,"price":300},
         {"color":"#a28481","value":40,"price":100},
         {"color":"#00A699","value":60,"price":50}
-    ]
+    ];
     graph.append('g')
         .selectAll('rect')
         .data(data_for_legend)
@@ -178,54 +173,53 @@ d3.csv('data/dataset/df.csv').then(data => {
         .attr('stroke-width', '0px')
         .style("anchor", "middle")
         .style("opacity", '0.6');
-
+    // legend segments line (colored square, which represent the price)
     svg.selectAll("legend")
         .data(data_for_legend)
         .enter()
         .append("line")
             .attr('x1', graphWidth + 50)
             .attr('x2', graphWidth + 70)
-            .attr('y1', function(d){ return 105 + d.value } )
-            .attr('y2', function(d){ return 105 + d.value } )
+            .attr('y1', d => 105 + d.value)
+            .attr('y2', d => 105 + d.value)
             .attr('stroke', '#767676')
             .attr('stroke-width', '2px')
-            .style('stroke-dasharray', ('2,2'))
-    // Add legend: labels
+            .style('stroke-dasharray', ('2,2'));
+    // legend labels (colored square, which represent the price)
     svg.selectAll("legend")
         .data(data_for_legend)
         .enter()
         .append("text")
             .attr('x', graphWidth + 80)
-            .attr('y', function(d){ return 105 + d.value })
+            .attr('y', d => 105 + d.value)
             .text(d => '$' + d.price)
-            .style("font-size", 12)
             .attr('alignment-baseline', 'middle')
             .attr('font-family', 'Arial')
             .attr("fill", "#767676")
+            .style("font-size", 12);
   
     // create axes groups
     const xAxisGroup = graph.append('g')
         .attr("class", "axis_x")
-        .attr('transform', `translate(0, ${graphHeight})`)
+        .attr('transform', `translate(0, ${graphHeight})`);
     const yAxisGroup = graph.append('g')
         .attr("class", "axis_y");
     xAxisGroup.call(d3.axisBottom(x));
     yAxisGroup.call(d3.axisLeft(y));
-
     // text label for the x axis
     svg.append("text")             
         .attr("transform", `translate(${graphWidth/2 + margin.left -5}, ${graphHeight + margin.top + 40})`)
+        .attr('font-family', 'Arial')
         .attr('fill', '#767676')
-        .style('font-size', '16px')
-        .style("text-anchor", "middle")
         .text("Reviews")
-        .attr('font-family', 'Arial');
+        .style('font-size', '16px')
+        .style("text-anchor", "middle");
     // text label for the y axis
     svg.append("text")             
         .attr("transform", `translate(40, ${graphHeight/2 + margin.top + 5})`)
+        .attr('font-family', 'Arial')
         .attr('fill', '#767676')
-        .style('font-size', '16px')
-        .style("text-anchor", "middle")
         .text("Days")
-        .attr('font-family', 'Arial');
+        .style('font-size', '16px')
+        .style("text-anchor", "middle");
 });
