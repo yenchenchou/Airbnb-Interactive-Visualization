5b534892a7c3b4389e2524d2ce827ff6fe3cb69c
[weifanchen] <[ap248166@gmail.com]>
2019-12-02 16:39:50 -0800

    fix filter function

--- project/my-app/src/views/Home.vue
+++ project/my-app/src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="home">
-    <!-- <h1>INF554 Project</h1>
-    <h2>include the link to our paper and video</h2> -->
-    <!-- <mainpage/> -->
     <h1 align="center">Deep Trip in California</h1>
     <table border="0">
       <tbody>
@@ -14,44 +11,19 @@
           <td class="right-part top">
                 <div>Room Type</div>
                 <div class="taskList" >
-                <!-- <div class="taskList" @click="handle1">  -->
-                  <!-- <input class="filter" type="radio" name="condition1" value="0"> All
-                  <input class="filter" type="radio" name="condition1" value="1"> Shared Room
-                  <input class="filter" type="radio" name="condition1" value="2"> Private Room
-                  <input class="filter" type="radio" name="condition1" value="3"> Entire Home/Apt -->
-                  <input type="radio"  name="task1" value="0"  v-model='selectStatus1'> All
-                  <input type="radio"  name="task1" value="1" v-model='selectStatus1'> Shared Room
-                  <input type="radio"  name="task1" value="2" v-model='selectStatus1'> Private Room
-                  <input type="radio"  name="task1" value="3" v-model='selectStatus1'> Entire Home/Apt
-                  <input class="forSearch" type="button" value="Search" @click="search1(selectStatus1)"/>
+                  <input type="radio"  name="task1" value=0  v-model='selectStatus1'> All
+                  <input type="radio"  name="task1" value=1 v-model='selectStatus1'> Shared Room
+                  <input type="radio"  name="task1" value=2 v-model='selectStatus1'> Private Room
+                  <input type="radio"  name="task1" value=3 v-model='selectStatus1'> Entire Home/Apt
+                  <!-- <input class="forSearch" type="button" value="Search" @click="search1(selectStatus1)"/> -->
                 </div>
                 <div>Instant Bookable</div>
                 <div class="taskList" >
-                <!-- <div class="taskList" @click="handle2">  -->
-                  <!-- <input class="filter" type="radio" name="condition2" value="0"> All
-                  <input class="filter" type="radio" name="condition2" value="1"> Bookable
-                  <input class="filter" type="radio" name="condition2" value="2"> Not Bookable -->
-                  <input type="radio"  name="task2" value="0" v-model='selectStatus2'> All
-                  <input type="radio"  name="task2" value="1" v-model='selectStatus2'> Bookable
-                  <input type="radio"  name="task2" value="2" v-model='selectStatus2'> Not Bookable
-                  <input class="forSearch" type="button" value="Search" @click="search2(selectStatus2)"/>
+                  <input type="radio"  name="task2" value=0 v-model='selectStatus2'> All
+                  <input type="radio"  name="task2" value=1 v-model='selectStatus2'> Bookable
+                  <input type="radio"  name="task2" value=2 v-model='selectStatus2'> Not Bookable
+                  <!-- <input class="forSearch" type="button" value="Search" @click="search2(selectStatus2)"/> -->
                 </div>
-                <!-- <div class="taskList" @click="handle1">
-                <label for="all">All</label>
-                <input type="radio" value="1" v-model="picked">
-                <label for="sharedroom">Shared Room</label>
-                <input type="radio" value="2" v-model="picked">
-                <label for="privateroom">Private Room</label>
-                <input type="radio" value="3" v-model="picked">
-                <label for="entirehomeapt">Entire Home/Apt</label>
-                <input type="radio" value=4 v-model="picked">
-                <span id='csv'>Picked: {{ picked }}</span>
-                </div> -->
-                <!-- filter function -->
-                <!-- <div type="hidden" id="detectChange">
-                  <input name="flag1" id="flag1"/>
-                  <input type="hidden" name="flag2" id="flag2"/>
-                </div> -->
           </td>
         </tr>
         <tr>
@@ -114,63 +86,42 @@ export default {
   methods:{
     forceRerender(){
         this.dataIsReady+=1
-        console.log(this.hotel.features.length)
-    },
-    search1(val){
-        console.log("search1",val)
-        // room type
-        var room_type_map = {1:"Shared room",2:"Private room",3:"Entire home/apt"}
-        var bookable_map = {1:"t",2:"f"}
-        var data = this.all_hotel
-        var temp=[];
-        if (this.selectStatus1==0 & this.selectStatus2==0){
-            this.hotel = this.all_hotel
-        } else if(this.selectStatus1==0) {
-            temp = data.features.filter(d=>d.properties.room_type==bookable_map[this.selectStatus2])
-        } else if(this.selectStatus2==0){
-            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
-        } else{
-            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
-            temp = temp.filter(d=>d.properties.instant_bookable==bookable_map[this.selectStatus2])
-        }
-        this.hotel = {type:"FeatureCollection",features: temp }
-        //console.log(room_type_map[this.selectStatus1],bookable_map[this.selectStatus2])
-        this.forceRerender()
-    },
-    search2(val){
-        console.log("search2",val)
-        var room_type_map = {1:"Shared room",2:"Private room",3:"Entire home/apt"}
-        var bookable_map = {1:"t",2:"f"}
-        var data = this.all_hotel
-        var temp=[];
-        if (this.selectStatus1==0 & this.selectStatus2==0){
-            this.hotel = this.all_hotel
-        } else if(this.selectStatus1==0) {
-            temp = data.features.filter(d=>d.properties.room_type==bookable_map[this.selectStatus2])
-        } else if(this.selectStatus2==0){
-            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
-        } else{
-            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
-            temp = temp.filter(d=>d.properties.instant_bookable==bookable_map[this.selectStatus2])
-        }
-        this.hotel = {type:"FeatureCollection",features: temp }
-        //console.log(room_type_map[this.selectStatus1],bookable_map[this.selectStatus2])
-        this.forceRerender()
+        console.log(this.selectStatus1,this.selectStatus2,this.hotel.features.length)
     },
     print_input(variable){
         this.selectedpoint = variable        
       },
+    filter_data(){
+        var room_type_map = {1:"Shared room",2:"Private room",3:"Entire home/apt"}
+        var bookable_map = {1:"t",2:"f"}
+        var data = this.all_hotel
+        var temp=[];
+        if (this.selectStatus1==0 & this.selectStatus2==0){
+            temp = data.features
+        } else if(this.selectStatus1==0) {
+            temp = data.features.filter(d=>d.properties.instant_bookable==bookable_map[this.selectStatus2])
+        } else if(this.selectStatus2==0){
+            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
+        } else{
+            temp = data.features.filter(d=>d.properties.room_type==room_type_map[this.selectStatus1])
+            temp = temp.filter(d=>d.properties.instant_bookable==bookable_map[this.selectStatus2])
+            
+        }
+        this.hotel = {type:"FeatureCollection",features: temp }
+        this.forceRerender()
+
+    }
     
   },
-// watch:{
-//     selectStatus1: function(newValue,oldValue){
-//         console.log("select1",newValue,oldValue)
-//         this.filter_data()
-//     },
-//     selectStatus2: function(newValue,oldValue){
-//         console.log("select2",newValue,oldValue)
-//         this.filter_data()
-//     },
-//     }
+watch:{
+    selectStatus1: function(newValue,oldValue){
+        console.log("select1",newValue,oldValue)
+        this.filter_data()
+    },
+    selectStatus2: function(newValue,oldValue){
+        console.log("select2",newValue,oldValue)
+        this.filter_data()
+    },
+    }
 }
 </script>
