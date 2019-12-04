9ab9f5db0fd72edd60903eb2d82c450176ec0344
CHOUYENCHEN <intelchou@gmail.com>
2019-12-04 00:06:58 -0800

    remove console.log synntax for deployment

--- project/my-app/src/views/Home.vue
+++ project/my-app/src/views/Home.vue
@@ -128,8 +128,8 @@ export default {
         // console.log(this.selectStatus1,this.selectStatus2,this.hotel.features.length)
     },
     filter_data(){
-        console.log(this.hotel)
-        console.log('filter_function',this.selectStatus1,this.selectStatus2,this.hotel.features.length)
+        // console.log(this.hotel)
+        // console.log('filter_function',this.selectStatus1,this.selectStatus2,this.hotel.features.length)
         var room_type_map = {1:"Shared room",2:"Private room",3:"Entire home/apt"}
         var bookable_map = {1:'t',2:'f'}
         var data = this.all_hotel
@@ -150,7 +150,7 @@ export default {
         this.forceRerender()
     },
     print_input(variable){
-        console.log("Home/print_input",variable)
+        // console.log("Home/print_input",variable)
         this.selectedpoint = variable
         //this.$emit("point_hometoplot",this.point)
         
